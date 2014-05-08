5468bfaa8e83153f456ee3ec6b695607b214b729
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-05-08 13:37:06 -0400

    added back facebook for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,17 @@ class User < ActiveRecord::Base
 	
   has_many :pictures, dependent: :destroy
  
-  
+  def self.from_omniauth(auth)
+  where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+end
+
+def self.create_from_omniauth(auth)
+  create! do |user|
+    user.provider = auth["provider"]
+    user.uid = auth["uid"]
+    user.name = auth["info"]["nickname"]
+  end
+end
   
 
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   get "resources" => "pages#resources"
 
 
-
+get 'auth/:provider/callback', to: 'sessions#create'
+get 'auth/failure', to: redirect('/')
+get 'signout', to: 'sessions#destroy', as: 'signout'
 end
