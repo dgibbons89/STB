aea58b309fd9d6f12876b79e9fb3bbc2eebe0422
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-05-15 18:35:23 -0400

    updated login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class AuthenticationsController < ActiveRecord::Base
  def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
