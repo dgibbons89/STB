e00f5809546cc6d485f9bb740425311eef64bd83
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-05-15 22:10:28 -0400

    updated private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,6 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
 
- private
-
-	def current_user
-  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
-	end
-	helper_method :current_user
 
 
 
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,7 +9,7 @@ if @user.persisted?
   set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
 else
   session["devise.facebook_data"] = request.env["omniauth.auth"]
-  ****redirect_to root_path****
+  redirect_to root_path
 end
  end
 
