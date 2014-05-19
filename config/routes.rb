d552310600e3884a5bb59921e3ca200d666603bc
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-05-19 15:47:12 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   resources :users
   resources :videos	
 
-  resources :friends, : only => [:index] do
-    post 'fb_friends', : on => :collection
+  resources :friends, only: => [:index] do
+    post 'fb_friends', on => :collection
   end
 end
   
