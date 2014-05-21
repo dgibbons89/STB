927363bf04cf642b37c1133a20169b1d9f9a4bad
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-05-21 12:36:06 -0400

    email option

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,6 +3,7 @@ class AuthenticationsController < ApplicationController
  def create
     @user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
+    UserMailer.signup_confirmation(@user).deliver
     redirect_to root_url
   end
 
