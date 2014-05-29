c0ed102d6587619d3b47689327dc325bbefaa666
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-05-29 14:26:31 -0400

    user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,5 +38,15 @@ end
  @friends = current_user.facebook.get_connections("me")   
 end
 
+  def self.create_with_omniauth(auth)  
+    create! do |user|
+      user.first_name = auth["user_info"]["first_name"]
+      user.last_name = auth["user_info"]["last_name"]
+      user.email = auth["user_info"]["email"]
+      user.fb_uid = auth["uid"]
+      user.fb_token = auth["credentials"]["token"]
+    end
+  end
+
   validates :name, presence: true
 end
