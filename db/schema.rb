6091df0c9e0a562ecda110146c4d6e9fc8476ac3
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-04 13:43:23 -0400

    video id to votes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522223347) do
+ActiveRecord::Schema.define(version: 20140604173855) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20140522223347) do
     t.integer  "picture_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "video_id"
   end
 
 end
