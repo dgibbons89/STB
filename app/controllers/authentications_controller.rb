8f7144546bccdc31fac0ba17f80f8da59dc9bca4
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-03 22:02:44 -0400

    fb

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -15,7 +15,15 @@ class AuthenticationsController < ApplicationController
     redirect_to root_url
   end
 
-  
+  def fb_friends
+    @user_fb_token = current_user.oauth_token
+
+    unless @user_fb_token.blank?
+      @fb_friends = FbGraph::User.me(@user_fb_token.access_token).friends
+      @fb_friends = @fb_friends.sort_by { |fb_frnd| fb_frnd.raw_attributes['name']}
+    end
+  end
+
 
   
 
