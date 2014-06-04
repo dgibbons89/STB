d992d74df95be9990d96347316072d24f5b1c7e5
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-04 19:57:50 -0400

    session helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
 
-def current_user
-    @current_user ||= User.find(authentication[:user_id]) if authentication[:user_id]
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 
