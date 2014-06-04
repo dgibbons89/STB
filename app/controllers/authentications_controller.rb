ece98636eaf20c2aee40257634926275c1225757
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-03 20:29:49 -0400

    added authentication controller lines

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,21 +1,28 @@
 class AuthenticationsController < ApplicationController
   before_filter :authenticate_user!
    def create
-      @user = User.from_omniauth(env["omniauth.auth"])
-      session[:user_id] = user.id
-      if @user.save
-        UserMailer.registration_confirmation(@user).deliver
-      redirect_to root_url
-      end
+    @user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    if @user.save
+      UserMailer.registration_confirmation(@user).deliver
+    redirect_to root_url
     end
+  end
 
 
-    def destroy
-      session[:user_id] = nil
-      redirect_to root_url
-    end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end
 
-    
+  def fb_friends
+  @user_fb_token = current_user.facebook_access_token
+
+  unless @user_fb_token.blank?
+    @fb_friends = FbGraph::User.me(@user_fb_token.access_token).friends
+    @fb_friends = @fb_friends.sort_by { |fb_frnd| fb_frnd.raw_attributes['name']}
+  end
+end
 
   
 
