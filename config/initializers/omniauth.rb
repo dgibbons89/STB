3ebff82fef8c2d7254079fae06b37a943cce7bbf
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-03 20:31:42 -0400

    deleted omni

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '675585185824524', 'aa5adc9f89aefb6240ad211b5519b209', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-end
