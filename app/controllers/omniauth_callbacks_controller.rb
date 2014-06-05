a8642d248ec5e54ca41a97a542bbe3e68a7d6924
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-05 17:34:08 -0400

    omni controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
 
- def all
+ def facebook
  # You need to implement the method below in your model (e.g. app/models/user.rb)
  	@user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
