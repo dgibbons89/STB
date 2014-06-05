4690ad3655382bf562c1a54dfe914b8ce0fb6e62
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-05 14:40:10 -0400

    private method, request oauth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:oauth_token]) if session[:oauth_token]
   end
   helper_method :current_user
 
