ac4fccada4bb10465478c468fa55bcb71dbd7204
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-05 14:27:53 -0400

    private helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
 
   
-
+  
 
 
   protected
@@ -19,10 +19,12 @@ class ApplicationController < ActionController::Base
   end
 
   
-  helper_method :current_user
+  
   private
-  
-  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 
   def record_not_found
     redirect_to action: :index
