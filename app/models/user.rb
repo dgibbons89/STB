857d03d4a98e2a6c18cd6242ada627752405922e
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-06 16:17:16 -0400

    user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,26 @@ class User < ActiveRecord::Base
   
   validates :name, presence: true
   
- 
+  def apply_omniauth(auth)
+  # In previous omniauth, 'user_info' was used in place of 'raw_info'
+  self.email = auth['extra']['raw_info']['email']
+  # Again, saving token is optional. If you haven't created the column in authentications table, this will fail
+  authentications.build(:provider => auth['provider'], :uid => auth['uid'], :token => auth['credentials']['token'])
+  end
+
+  def self.find_or_create_by_facebook(data)
+    self.find_or_create_by_facebook_id(data['extra']['raw_info']['id'], :facebook_token => data['credentials']['token'])
+  end
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+  data = access_token.extra.raw_info
+  if data == "false"
+    self.new
+  elsif user = self.find_by_email(data.email)
+    user
+  else # Create a user with a stub password. 
+    self.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
+  end
+end
 
 
    def self.from_omniauth(auth)
