8def9b7ac91881ab876648893e4cb309adca3220
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-11 18:29:32 -0400

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -25,10 +25,7 @@ Rails.application.routes.draw do
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   root "pictures#index"
   get "faq" => "pages#faq"
-  get "facebook" => "pages#facebook"
-
-  get '/auth/:provider/callback' => 'authentications#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'authentications#destroy', as: 'signout'
+  
+  
 
 end
