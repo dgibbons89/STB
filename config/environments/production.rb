45c2f8559e2385df48d3b18abe846e762691a587
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-12 18:23:46 -0400

    production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,15 +66,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', 
-    port: '587',
-    enable_starttls_auto: true,
-    user_name: 'someuser',
-    password: 'somepass',
-    authentication => :plain,
-    domain => 'stolobu.com'
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com', 
+  #   port: '587',
+  #   enable_starttls_auto: true,
+  #   user_name: 'someuser',
+  #   password: 'somepass',
+  #   authentication => :plain,
+  #   domain => 'stolobu.com'
+  # }
   config.action_mailer.default :charset => "utf-8"
  
 
