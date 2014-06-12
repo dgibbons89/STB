9cc7f36721c2599e5cfa1861051d045dc8a5ddea
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-12 18:04:17 -0400

    devise

--- config/initializers/constants.rb
+++
@@ -1,7 +0,0 @@
-APP_ID= '675585185824524' # please change!
-APP_SECRET= 'aa5adc9f89aefb6240ad211b5519b209' # please change!
-SITE_URL = 'www.stolobu.com/' # please change!
-
-Pusher.app_id = ENV["pusher_app_id"]
-Pusher.key    = ENV["pusher_key"]
-Pusher.secret = ENV["pusher_secret"]
