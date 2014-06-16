5242cde081f0235153e76a8194d8f0c947f9895b
Dean Gibbons <dgibbons89@gmail.comgit config -- user.email dgibbons89@gmail.comgit config --global user.email dgibbons89@gmail.com>
2014-06-16 15:19:22 -0400

    deleted scope

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,8 +3,7 @@
 
 require 'omniauth-facebook'
 Devise.setup do |config|
-   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], 
-  :scope => 'email, offline_access, user_birthday, user_friends, read_friendlists, friends_birthday'
+   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
